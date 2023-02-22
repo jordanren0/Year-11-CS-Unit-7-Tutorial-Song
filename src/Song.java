607e81b8250fc66a623d3b0c071e2925a792da65
jordan_ren <jordanr2025@student.cis.edu.hk>
2023-02-22 22:48:19 +0800

    finished

--- src/Song.java
+++ src/Song.java
@@ -1,3 +1,32 @@
-public class Song {
+import java.util.ArrayList;
 
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> newArr;
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        newArr = new ArrayList<>();
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> name){
+        name.replaceAll(String::toLowerCase);
+        int total = 0;
+        for(int i = 0; i < name.size(); i++){
+            if(!newArr.contains(name.get(i))){
+                total++;
+                newArr.add(name.get(i));
+            }
+        }
+        return total;
+    }
 }
--- src/SongTester.java
+++ src/SongTester.java
@@ -9,13 +9,13 @@ public class SongTester {
 
     @org.junit.jupiter.api.Test
     public void TestForTitleAndArtist() {
-        assertEquals("Wrong title!", "Lavender Haze", song.getTitle());
-        assertEquals("Wrong artist!", "Taylor Swift", song.getArtist());
+        assertEquals("Lavender Haze", song.getTitle(), "Wrong title!");
+        assertEquals("Taylor Swift", song.getArtist(), "Wrong title!");
     }
 
     @org.junit.jupiter.api.Test
     public void TestForDataEncapsulation() {
-        assertEquals("Make sure that your attributes are private!", 0, song.getClass().getFields().length);
+        assertEquals(0, song.getClass().getFields().length, "Make sure that your attributes are private!");
     }
 
     @org.junit.jupiter.api.Test
